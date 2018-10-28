be3e9b00d10e95f47be411b5c6c626c8e98b794d
randy0815 <kaefer.andreas@freenet.de>
2018-10-28 14:16:39 +0100

    Add regular cost and income support

--- main.py
+++ main.py
@@ -24,54 +24,55 @@ def main():
     SpareTimeList = extractitemsfromknownList('KnownSpareTime.csv')
     CarList = extractitemsfromknownList('KnownCar.csv')
     TravelingList = extractitemsfromknownList('KnownTraveling.csv')
-    # MiscellaneousList = extractitemsfromknownList('KnownMiscellaneous.csv')
+    RegularCostsList = extractitemsfromknownList('KnownRegularCosts.csv')
     ClothingList = extractitemsfromknownList('KnownClothing.csv')
+    einkauflist = extractitemsfromknownList('Einkauf.csv')
 
     open('Parsed.csv', 'w').close()
-    with open('Umsatzanzeige.csv', newline='') as BankStatementCSV, open ('Parsed.csv', 'w') as Parsed,open ('Einkauf.csv') as einkauf:
-        Parsed.write('Datum,'+ FoodGroup + HygieneGroup + SparetimeGroup + CarGroup + 
+    with open('Umsatzanzeige1.csv', newline='') as BankStatementCSV, open ('Parsed.csv', 'w') as ParsedCSV, open('RegluarCosts.csv','w') as regularcostsCSV, open('Income.csv','w') as IncomeCSV :
+        ParsedCSV.write('Datum,'+ FoodGroup + HygieneGroup + SparetimeGroup + CarGroup + 
         TravelingGroup + MiscellaneousGroup + ClothingGroup +'\n')
 
-        Parsed.write('Datum,' + 7*SubHeadings + '\n')
-        spamreader = csv.reader(BankStatementCSV, delimiter=';', quotechar='|')
-        einkaufitems = csv.reader(einkauf, delimiter=';', quotechar='|')
-        for einkauflist in einkaufitems:
-            einkauflist
-
-        for row in spamreader:
+        ParsedCSV.write('Datum,' + 7*SubHeadings + '\n')
+        tBankStatement = csv.reader(BankStatementCSV, delimiter=';', quotechar='|')
+        for row in tBankStatement:
             # print (row[0]+ "\n")
             row[2] = row[2].replace("\"", "")
+            row[5] = row[5].replace(".", "")
             row[5] = row[5].replace(",", ".")
             bHandled = False
+            if float(row[5]) > 0:
+                IncomeCSV.write(row[0] + "," + row[2] + ','+ row[5] + "\n")
             for item in einkauflist:
                 if item in row[2]:
-                    Parsed.write(row[0] + "," + row[2] + ','+ row[5] + "\n")
+                    ParsedCSV.write(row[0] + "," + row[2] + ','+ row[5] + "\n")
                     bHandled = True
             for item in hygieneList:
                 if item in row[2]:
-                    Parsed.write(row[0] + 3 * ',' + "," + row[2] + ','+ row[5] + "\n")
+                    ParsedCSV.write(row[0] + 3 * ',' + "," + row[2] + ','+ row[5] + "\n")
                     bHandled = True
             for item in SpareTimeList:
                 if item in row[2]:
-                    Parsed.write(row[0] + 2*3 * ',' + "," + row[2] + ','+ row[5] + "\n")
+                    ParsedCSV.write(row[0] + 2*3 * ',' + "," + row[2] + ','+ row[5] + "\n")
                     bHandled = True
             for item in CarList:
                 if item in row[2]:
-                    Parsed.write(row[0] + 3*3* ',' + "," + row[2] + ','+ row[5] + "\n")
+                    ParsedCSV.write(row[0] + 3*3* ',' + "," + row[2] + ','+ row[5] + "\n")
                     bHandled = True
             for item in TravelingList:
                 if item in row[2]:
-                    Parsed.write(row[0] + 4*3* ',' + "," + row[2] + ','+ row[5] + "\n")
+                    ParsedCSV.write(row[0] + 4*3* ',' + "," + row[2] + ','+ row[5] + "\n")
                     bHandled = True
-            # for item in MiscellaneousList:
-            #     if item in row[2]:
-            #         Parsed.write(row[0] + 5*3* ',' + "," + row[2] + ','+ row[5] + "\n")
+            for item in RegularCostsList:
+                if item in row[2]:
+                    regularcostsCSV.write(row[0] + ',' + "," + row[2] + ','+ row[5] + "\n")
+                    bHandled = True
             for item in ClothingList:
                 if item in row[2]:
-                    Parsed.write(row[0] + 5*3* ',' + "," + row[2] + ','+ row[5] + "\n")
+                    ParsedCSV.write(row[0] + 5*3* ',' + "," + row[2] + ','+ row[5] + "\n")
                     bHandled = True
             if (bHandled == False):
-                Parsed.write(row[0] + 3 * 6 * ',' + "," + row[2] + ',' + row[5] + "\n")
+                ParsedCSV.write(row[0] + 3 * 6 * ',' + "," + row[2] + ',' + row[5] + "\n")
 
 if __name__ == "__main__":
     main()
