a8fc073641a0a40bea99659a8ae9492925c512e0
randy0815 <kaefer.andreas@freenet.de>
2019-04-19 16:29:00 +0200

    Fix formatting and sorting

--- BankStatementParser/BankStatementParser.py
+++ BankStatementParser/BankStatementParser.py
@@ -1,5 +1,6 @@
 import csv
 
+CSVLineOffset = 14
 
 class BankStatementParser:
 
@@ -11,10 +12,37 @@ class BankStatementParser:
                 lines.append(List)
         return lines
 
+    def extract_items_from_bankstatement_list(self, csvlist=''):
+        lines = list()
+        with open(csvlist, newline='') as KnownList:
+            items = csv.reader(KnownList, delimiter=';', quotechar='|')
+            for List in items:
+                if items.line_num > CSVLineOffset:
+                    lines.append(List)
+        return lines
+
+
     def parse_list(self, parameter_list, ParsedCSV, row, i):
+        '''
+
+        :param parameter_list:
+        :type parameter_list:
+        :param ParsedCSV:
+        :type ParsedCSV:
+        :param row:
+        :type row:
+        :param i:
+        :type i:
+        :return:
+        :rtype:
+        '''
         bHandled = False
         for item in parameter_list:
             if item in row[2]:
-                ParsedCSV.write(row[0] + i * ';' + ';' + item + ';' + row[5] + "\n")
+                if 'VISA' in row[2]:
+                    row[2].replace('VISA','')
+                    ParsedCSV.write(row[0] + i * ';'*3 + ';' + str(item).capitalize() + ';' + str(row[5]).replace('-','') + ';V' + "\n")
+                else:
+                    ParsedCSV.write(row[0] + i * ';'*3 + ';' + str(item).capitalize() + ';' + str(row[5]).replace('-','') + ';LV'+ "\n")
                 bHandled = True
         return bHandled
--- main.py
+++ main.py
@@ -17,29 +17,32 @@ def main():
     # open('Parsed.csv', 'w').close()
     with open('Parsed.csv', 'w') as ParsedCSV, open('RegluarCosts.csv', 'w') as regularcostsCSV, open('Income.csv',
                                                                                                       'w') as IncomeCSV, open(
-        'RegularSavings.csv', 'w') as RegularSavings:
+        'RegularSavings.csv', 'w') as RegularSavings, open('BarAuszahlung.csv', 'w') as barCSV:
 
         writeHeadings(ParsedCSV)
 
         WriteParsedDebits(CategoryList, IncomeCSV, ParsedCSV, RegularCostsList, RegularSavings, namelist, parser,
-                          regularcostsCSV, tBankStatement)
+                          regularcostsCSV, tBankStatement, barCSV)
 
 
 def WriteParsedDebits(CategoryList, IncomeCSV, ParsedCSV, RegularCostsList, RegularSavings, namelist, parser,
-                      regularcostsCSV, tBankStatement):
+                      regularcostsCSV, tBankStatement, barCSV):
+    print("Locations:" + str(CategoryList))
+
     for row in tBankStatement:
         bHandled = False
         if float(row[5].replace(",", ".")) > 0:
             #row[5] =
             IncomeCSV.write(row[0] + separator + row[2] + separator + row[5] + "\n")
             bHandled = True
-        if "BARGELDAUSZAHLUNG" in row[4]:
-            ParsedCSV.write(row[0] + 7 * 3 * separator + separator+separator + row[2] + separator + row[5] + "\n")
-        for x in range(0, 6):
+        if "BARGELDAUSZAHLUNG" in row[4] or "Bargeldauszahlung" in row[4]:
+            barCSV.write(row[0] + 7 * 3 * separator + separator+separator + row[2] + separator + row[5] + "\n")
+            bHandled = True
+        for x in range(0, len(CategoryList)):
             bHandled |= parser.parse_list(CategoryList[x], ParsedCSV, row, x)
         for item in RegularCostsList[0]:
             if item in row[2]:
-                regularcostsCSV.write(row[0] + separator + separator + item + separator + row[5] + "\n")
+                regularcostsCSV.write(row[0] + separator + separator + item + separator + str(row[5]).replace('-','') + "\n")
                 bHandled = True
         if (bHandled == False):
             if (namelist[0][0] in row[2]) or (namelist[0][1] in row[2]):
@@ -47,25 +50,24 @@ def WriteParsedDebits(CategoryList, IncomeCSV, ParsedCSV, RegularCostsList, Regu
             else:
                 if ("AMAZON" in row[2]):
                     row[2] = "AMAZON"
-                ParsedCSV.write(row[0] + 3 * 6 * separator + separator + row[2] + separator + row[5] + "\n")
+                ParsedCSV.write(row[0] + 3 * 5 * separator + separator + str(row[2]).capitalize() + separator + str(row[5]).replace('-','') + "\n")
 
 
 def writeHeadings(parsedCSV):
-    FoodGroup = 'Einkauf;Essen;%;'
+    food_group = 'Einkauf;Essen;%;'
     HygieneGroup = 'Beauty;Hygiene;%;'
-    SparetimeGroup = 'Freizeit;Restaurant,%,'
-    CarGroup = 'Auto;%;%;'
-    TravelingGroup = 'Tickets;Reisekosten;%;'
+    SparetimeGroup = 'Freizeit;Restaurant;%;'
+    Mobility_Group = 'Auto;Tickets;Reisekosten;'
     MiscellaneousGroup = 'Sonstiges;%;%;'
-    ClothingGroup = 'Kleidung;Schmuck;%'
+    ClothingGroup = 'Kleidung;Schmuck;%;'
     SubHeadings = 'Ort;Betrag;ZA;'
-    parsedCSV.write('Datum;' + FoodGroup + HygieneGroup + SparetimeGroup + CarGroup +
-                    TravelingGroup + MiscellaneousGroup + ClothingGroup + '\n')
+    parsedCSV.write('Datum;' + food_group + HygieneGroup + SparetimeGroup + Mobility_Group
+                     + ClothingGroup + MiscellaneousGroup + '\n')
     parsedCSV.write('Datum;' + 7 * SubHeadings + '\n')
 
 
 def DateSort(e):
-    print(parser.parse(e[1],dayfirst=True))
+    #print(parser.parse(e[1],dayfirst=True))
     return parser.parse(e[1],dayfirst=True)
 
 
@@ -74,11 +76,8 @@ def PrepareBankStatement(parser):
     files = glob.glob(filepath)
     tBankStatement = list()
     for file in files:
-        tBankStatement.extend(parser.extract_items_from_known_list(file))
-        print(tBankStatement)
-        print("\n")
-    tBankStatement.sort(reverse= True, key=DateSort)
-    print(tBankStatement)
+        tBankStatement.extend(parser.extract_items_from_bankstatement_list(file))
+    tBankStatement.sort(reverse= False, key=DateSort)
     for row in tBankStatement:
         row[2] = row[2].replace("\"", "")
         row[2] = row[2].replace(",", ".")
